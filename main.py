c1b971161cd9865adcabc92054e5f827c1dd13b1
Justin Kopcinski <jkopcin@gmail.com>
2024-04-25 17:39:55 -0700

    -bug fixes

--- main.py
+++ main.py
@@ -25,27 +25,23 @@ eqns = {
 }
 
 if __name__ == "__main__":
+
     bitString = generate_random_string(NUM_BITS)
     bitList = split_bitstring(bitString)
 
-    print(bitList) #debug
-
     vals = [int(i, 2) for i in bitList] #convert bitStrings to integers
-
-    print(vals) #debug
 
     objectList = [None] * ((2**ATTRIBUTES) - 1) #initialize object list to empty
 
     #-------------------------DEBUG------------------
-    print(objectList) 
-    print(len(objectList))
+    print("bitString: " + bitString)
+    print(bitList)
+    print(vals)
     #-------------------------DEBUG------------------
 
     #BUILD OBJECT LIST ----- MOVE TO FUNCTION
     for i in vals[::3]: #use list slicing to find each set
-        print(i)
         if not objectList[i] and i == vals[0]: #check if there is an object at this position of the set
-            print("here")
             object_response = input("Do you see any objects in the problem? ") #ask about the object
             if(object_response == 'y' or object_response == "yes"): #check if response is affirmative
                 object_name = input("What is the object? ")
@@ -53,7 +49,7 @@ if __name__ == "__main__":
             else:
                 break
         else:
-            object_response = input("Do you see any objects in the problem? ") #ask about the object
+            object_response = input("Do you see any more objects in the problem? ") #ask about the object
             if(object_response == 'y' or object_response == "yes"): #check if response is affirmative
                 object_name = input("What is the object? ")
                 objectList[vals[i]] = object_name
